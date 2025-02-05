234b8001aa199273237fcded292947771437f27f
Thoria <113485245+thoriataher@users.noreply.github.com>
2025-02-05 15:11:44 +0200

    feat: register company and event blueprints, update auth route
    Added:
    - Registered company_bp under /api/v1/company
    - Registered event_bp under /api/v1/events
    Modified:
    - Changed auth_bp route from /auth to /api/v1/companies
    Removed:
    - Old /auth route

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_session import Session
 from config.config import config
 from routes.auth_routes import auth_bp
+from routes.company_routes import company_bp
+from routes.event_routes import event_bp
 
 app = Flask(__name__)
 app.config.from_object(config["developement"])
 Session(app)
 
-app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(auth_bp, url_prefix="/api/v1/companies")
+app.register_blueprint(company_bp, url_prefix="/api/v1/company")
+app.register_blueprint(event_bp, url_prefix="/api/v1/events")
+
 
 
 if __name__ == "__main__":
